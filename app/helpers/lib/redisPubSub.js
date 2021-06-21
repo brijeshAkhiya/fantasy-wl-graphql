645d04536383fd8f9958e96f6729c470dc286201
yudiz_brijesh <brijesh.a@yudiz.in>
2021-06-21 17:13:36 +0530

    As redis was not supporeted on heroku used normal pubsub class

--- app/helpers/lib/redisPubSub.js
+++ app/helpers/lib/redisPubSub.js
@@ -1,17 +1,21 @@
-const { RedisPubSub } = require('graphql-redis-subscriptions');
-const Redis = require('ioredis');
+// const { RedisPubSub } = require('graphql-redis-subscriptions');
+// const Redis = require('ioredis');
 
-const options = {
-    host: '127.0.0.1',
-    port: 6379,
-    retryStrategy: (times) =>
-        // reconnect after
-        Math.min(times * 50, 2000),
-};
+// const options = {
+//     host: '127.0.0.1',
+//     port: 6379,
+//     retryStrategy: (times) =>
+//         // reconnect after
+//         Math.min(times * 50, 2000),
+// };
 
-const pubsub = new RedisPubSub({
-    publisher: new Redis(options),
-    subscriber: new Redis(options),
-});
+// const pubsub = new RedisPubSub({
+//     publisher: new Redis(options),
+//     subscriber: new Redis(options),
+// });
+
+const { PubSub } = require('apollo-server-express');
+
+const pubsub = new PubSub();
 
 module.exports = pubsub;
